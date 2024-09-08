e11e2b0201ef79b507c585f2e7558c2d490e070a
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-09-08 19:26:29 +0100

    'profile'

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,11 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native';
 import Test from './Test'
-
+import { Link } from 'expo-router';
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>How are you?</Text>
       <Test/>
+      <Link href="/Test" style={{color: 'blue'}}>Go to profile</Link>
     </View>
   );
 }
--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,14 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const test = () => {
+const profile = () => {
   return (
     <View>
-      <Text>test</Text>
+      <Text>profile</Text>
     </View>
   )
 }
 
-export default test
+export default profile
 
 const styles = StyleSheet.create({})
