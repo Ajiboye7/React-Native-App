67992e44e1ee12c3abc6de31b5a3cce42eec9c61
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-09-09 18:39:37 +0100

    'tabs'

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Home = () => {
   return (
     <View>
-      <Text>Home</Text>
+     <Text className="text-3xl mb-10 font-extrabold text-white">This is Home page</Text>
     </View>
   )
 }
