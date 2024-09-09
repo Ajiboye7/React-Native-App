eba655842dd968899dca3507589bd2e67b8bf19b
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-09-09 13:46:09 +0100

    'taiwind'

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,25 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import Test from './Test'
 import { Link } from 'expo-router';
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>How are you?</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl mb-10">How are you?</Text>
       <Test/>
       <Link href="/profile" style={{color: 'blue'}}>Go to profile</Link>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-  },
-  text: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-});
