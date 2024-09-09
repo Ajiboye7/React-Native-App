100aad4a46c4d424eb8772715f4b247393dfb0bd
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-09-09 17:25:12 +0100

    'screens'

--- app/(tabs)/Test.jsx
+++ app/(tabs)/Test.jsx
@@ -1,18 +1,3 @@
-/*import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-
-export class test extends Component {
-  render() {
-    return (
-      <View>
-        <Text style={{color:'green', display:'flex', alignItems:'center', justifyContent:'center'}}> textInComponent </Text>
-      </View>
-    );
-  }
-}
-
-export default test;*/
-
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
