958172a4cecdefa4e2b3548a06097c94f94d8bb1
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-09-10 18:17:17 +0100

    'form'

--- components/FormField.jsx
+++ components/FormField.jsx
@@ -1,5 +1,7 @@
-import { View, Text,TextInput } from 'react-native'
+import { View, Text,TextInput,Image,TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
+
+import { icons } from '../constants'
 
 const FormField = ({title, value,placeholder,handleChangeText,otherStyles,keyboardType='default', ...props}) => {
 
@@ -19,48 +21,19 @@ const FormField = ({title, value,placeholder,handleChangeText,otherStyles,keyboa
           keyboardType={keyboardType}
           secureTextEntry={title === 'Password' && !showPassword} 
       />
+      {title === 'Password' && (
+        <TouchableOpacity onPress={()=>
+        setShowPassword(!showPassword)}>
+        <Image
+            source={!showPassword ? icons.eye : icons.eyeHide}
+            className="w-6 h-6"
+            resizeMode='contain'/>
+        </TouchableOpacity>
+      )}
       </View>
     </View>
   )
 }
 
 export default FormField
-
-/*
-import { View, Text, TextInput } from 'react-native';
-import React, { useState } from 'react';
-
-const FormField = ({
-  title,
-  value,
-  placeholder,
-  handleChangeText,
-  otherStyles,
-  keyboardType = 'default',
-  ...props
-}) => {
-  const [showPassword, setShowPassword] = useState(false);
-
-  return (
-    <View className={`space-y-2 ${otherStyles}`}>
-      <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
-      <View className="border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center flex-row">
-        <TextInput
-          className="flex-1 text-white font-psemibold text-base"
-          value={value}
-          placeholder={placeholder}
-          placeholderTextColor="#7b7b8b"
-          onChangeText={handleChangeText} // Ensure it's `onChangeText`
-          keyboardType={keyboardType}
-          secureTextEntry={title === 'Password' && !showPassword} // Correct case for 'Password'
-          {...props}
-        />
-      </View>
-    </View>
-  );
-};
-
-export default FormField;
-
-*/
 
