30d556f892ba2b36259dcd526be4d3c057249d5f
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-09-10 12:21:42 +0100

    'index'

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,37 @@
-import { Text, View } from 'react-native';
-import { Link } from 'expo-router';
+import { Text, View, ScrollView, Image } from "react-native";
+import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
 export default function HomeScreen() {
   return (
-    <View className="flex items-center justify-center bg-white">
-      <Text className="text-3xl mb-10 font-extrabold">How are you?</Text>
-      <Link href="/home" style={{color: 'blue'}}>Go to Home</Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full justify-center items-center h-full px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+          
+        <View className="relative mt-5">
+          <Text className="text-3xl text-white font-bold text-center">
+            Discover Endless possibilities with {' '}
+             <Text className="text-secondary-200">Aora</Text>
+          </Text>
+          <Image
+            source={images.path}
+            className="absolute w-[135px] h-[15px] -bottom-2 -right-8"
+            resizeMode="contain"
+          />
+        </View>
+        </View>
+
+      </ScrollView>
+    </SafeAreaView>
   );
 }
-
