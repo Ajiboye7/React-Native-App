046711a5808668fedf46c611b26055e2693e9f78
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-09-11 17:45:53 +0100

    'sdk'

--- library/appwrite.js
+++ library/appwrite.js
@@ -1,8 +1,20 @@
-export const appwriteConfig ={
+import { Client } from 'react-native-appwrite';
+
+export const Config ={
     endpoint: 'http://cloud.appwrite.io/v1',
     platform: 'com.ajiboyedev.aora',
     projectId: '66e168a900130ea9ac05',
     databaseId: '66e16c380011e90b87d8',
     userCollectionId: '66e16cbd00119938ab11',
-    videoCollectionId:'66e16d16001f8cb68304'
+    videoCollectionId:'66e16d16001f8cb68304',
+    storageId:'66e171cd000fa61e0aba'
 }
+
+
+// Init React Native SDK
+const client = new Client();
+client
+    .setEndpoint(Config.endpoint) 
+    .setProject(Config.projectId) 
+    .setPlatform(Config.platform)
+;
