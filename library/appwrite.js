9b37e948bcdcfcf6ba898f3b939730798419bc5e
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-09-12 16:35:27 +0100

    'fixed'

--- library/appwrite.js
+++ library/appwrite.js
@@ -51,12 +51,4 @@ export const createUser = async (email, password, username) => {
   }
 };
 
-export const signIn = async (email, password) => {
-  try {
-    const session = await account.createEmailPasswordSession(email, password);
-    return session;
-  } catch (error) {
-    console.log(error);
-    throw new Error(error.message);
-  }
-};
+export const signIn = async (em
