490e3bbc13df200debaf80b170c2c5403c4f63a9
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-09-12 11:30:14 +0100

    'database'

--- library/appwrite.js
+++ library/appwrite.js
@@ -1,4 +1,5 @@
-import { Client, Account,ID } from "react-native-appwrite";
+import { Client, Account, Avatars, ID, Databases } from "react-native-appwrite";
+import SignIn from "../app/(auth)/sign-in";
 
 
 export const Config = {
@@ -21,30 +22,47 @@ client
   
 
 const account = new Account(client);
+const avatars = new Avatars(client)
+const databases = new Databases(client)
 
 
-export const createUser = () => {
-  account.create(ID.unique(), "me@example.com", "password", "Jane Doe").then(
-    function (response) {
-      console.log(response);
-    },
-    function (error) {
-      console.log(error);
-    }
-  );
+export const createUser = async (email, password, username) => {
+  try{
+    const newAccount = await account.create(ID.unique(), email, password, username)
+
+    if(!newAccount) throw Error
+
+    const avatarUrl = avatars.getInitials(username)
+    await SignIn(email,password);
+
+    const newUser = await databases.createDocument(
+        Config.databaseId,
+        config.userCollectionId,
+        ID.unique(),
+        {
+            accountId: newAccount.$id,
+            email,
+            username,
+            avatar:avatarUrl
+        }
+
+    )
+
+
+  }catch(error){
+    console.log(error);
+    throw new Error(error)
+  }
 };
 
+ 
+export const signIn = async (email, password)=>{
+    try{
+        const session = await account.createEmailSession(email, password)
 
-/*
-export const createUser = (email, password, username) => {
-  account.create(ID.unique(), email, password, username).then(
-    function (response) {
-      console.log(response);
-    },
-    function (error) {
-      console.log(error);
+        return session
+    }catch(error){
+        console.log(error)
+        throw new Error(error)
     }
-  );
-};
-
- */
+}
