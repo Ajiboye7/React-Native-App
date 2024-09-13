8603c79b967ef88686364e564d1da88218083c5b
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-09-13 17:40:53 +0100

    'context'

--- context/GlobalProvider.js
+++ context/GlobalProvider.js
@@ -1,1 +1,26 @@
 import { createContext, useContext, useState, useEffect } from "react";
+
+GlobalContext = createContext();
+
+export const useGlobalContext = () => useContext(GlobalContext);
+
+const GlobalProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+
+  useEffect(()=>{
+
+  },[])
+
+
+
+
+  return <GlobalContext.provider
+  value={{
+
+  }}>
+
+  </GlobalContext.provider>;
+};
