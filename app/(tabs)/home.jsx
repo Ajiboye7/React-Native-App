d781efb7e67a4af5906727049e1a4745960bf165
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-09-15 12:49:29 +0100

    'error'

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,11 +1,5 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
-const Home = () => {
-  return (
-    <View>
-     <Text className="text-3xl mb-10 font-extrabold text-white flex items-center justify-center">This is Home page</Text>
-    </View>
+import React fro
   )
 }
 
