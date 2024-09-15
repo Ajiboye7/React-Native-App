12a44d50e0b9a7c41d628bb51633c8c443ce3aa4
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-09-15 21:04:09 +0100

    'global'

--- context/GlobalProvider.js
+++ context/GlobalProvider.js
@@ -8,24 +8,7 @@ export const useGlobalContext = () => useContext(GlobalContext);
 
 export const GlobalProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [user, setUser] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-
-
-  useEffect(() => {
-    // Call the getCurrentUser function
-    getCurrentUser()
-      .then((res) => {
-        if (res) {
-          setIsLoggedIn(true);
-          setUser(res);
-        } else {
-          setIsLoggedIn(false);
-          setUser(null);
-        }
-      })
-      .catch((error) => {
-        console.error('Error fetching current user:', error);
+  const [user, s;
         setIsLoggedIn(false); 
         setUser(null); 
       })
