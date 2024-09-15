b55846462d9772a6b87f233945684d0d4c46c97b
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-09-15 21:03:41 +0100

    'gte'

--- app/_layout.js
+++ app/_layout.js
@@ -7,6 +7,30 @@ import GlobalProvider from '../context/GlobalProvider'
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded,error] = useFonts({
+    "poppins-Black": require('../assets/fonts/Poppins-Black.ttf'),
+    "poppins-Bold" :require("../assets/fonts/Poppins-Bold.ttf"),
+    "poppins-ExtraBold" :require("../assets/fonts/Poppins-ExtraBold.ttf"),
+    "poppins-ExtraLight" :require("../assets/fonts/Poppins-ExtraLight.ttf"),
+    "poppins-Light" :require("../assets/fonts/Poppins-Light.ttf"),
+    "poppins-Medium" :require("../assets/fonts/Poppins-Medium.ttf"),
+    "poppins-Regular" :require("../assets/fonts/Poppins-Regular.ttf"),
+    "poppins-SemiBold" :require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "poppins-Thin" :require("../assets/fonts/Poppins-Thin.ttf"),
+  }); 
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
     return null;
   } 
 
