a42ba7561d0da9e8c138d0e3982aa6879edc7910
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-09-17 11:10:15 +0100

    'commit'

--- context/GlobalProvider.js
+++ context/GlobalProvider.js
@@ -1,5 +1,33 @@
 import { createContext, useContext, useState, useEffect } from "react";
-impo]
+import { getCurrentUser } from "../library/appwrite";
+
+
+const GlobalContext = createContext();
+
+export const useGlobalContext = () => useContext(GlobalContext);
+
+export const GlobalProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+
+
+
+  useEffect(() => {
+    // Call the getCurrentUser function
+     getCurrentUser()
+      .then((res) => {
+        if (res) {
+          setIsLoggedIn(true);
+          setUser(res);
+        } else {
+          setIsLoggedIn(false);
+          setUser(null);
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching current user:', error);
         setIsLoggedIn(false); 
         setUser(null); 
       })
