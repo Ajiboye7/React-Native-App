078ccb79613cee17b2ae2055e15e2241cfbada73
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-09-23 12:36:33 +0100

    'videoCard'

--- components/VideoCard.jsx
+++ components/VideoCard.jsx
@@ -1,12 +1,34 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text,Image } from "react-native";
+import React from "react";
 
-const VideoCard = ({video:{title, thumbnail, video, creator:{username, avatar}}}) => {
+const VideoCard = ({
+  video: {
+    title,
+    thumbnail,
+    video,
+    creator: { username, avatar },
+  },
+}) => {
   return (
     <View className="flex-col items-center px-4 mb-14">
-      <Text className="text-2xl text-white">{title}</Text>
+      <View className="flex-row gap-3 items-start">
+        <View className="justify-center items-center flex-row flex-1">
+          <View className="w-[46px] h-[46px] rounded-lg border border-secondary justify-center items-center p-0.5">
+            <Image
+                source={{uri: avatar}}
+                className="w-full h-full rounded-lg"
+                resizeMode="cover"
+            />
+          </View>
+          <View className="justify-center flex-1 ml-3 gap-y-1">
+          <Text className="text-white font-psemibold text-sm">
+            {title}
+          </Text>
+          </View>
+        </View>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default VideoCard
+export default VideoCard;
