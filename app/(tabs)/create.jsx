bbc40180bc325f09e2ff4513dac35594a0b385f4
Ajiboye <ajiboyemuyideen7@gmail.com>
2024-09-25 18:37:53 +0100

    'create'

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -5,7 +5,69 @@ import { ScrollView } from "react-native";
 import FormField from "../../components/FormField";
 import CustomButton from "../../components/CustomButton";
 import { TouchableOpacity } from "react-native";
-humbnail.uri}}
+import { Video, ResizeMode } from "expo-av";
+import { icons } from "../../constants";
+
+const create = () => {
+  const [uploading, setUploading] = useState(false);
+  const [form, setForm] = useState({
+    title: "",
+    video: null,
+    thumbnail: null,
+    prompt: "",
+  });
+
+  const openPicker = async (selectType)=>{
+    
+  }
+  const submit =()=>{
+
+  }
+  return (
+    <SafeAreaView className="h-full">
+      <ScrollView className="px-4 my-6">
+        <Text className="text-2xl text-white font-psemibold">Upload Video</Text>
+        <FormField
+          title="Video Title"
+          value={form.title}
+          placeholder="Give your Video a catchy title"
+          handleChangeText={(e) => setForm({ ...form, title: e })}
+          otherStyles="mt-10"
+        />
+        <View className="mt-7 space-y-2">
+          <Text className="text-base text-gray-100 font-pmedium">
+            Upload Video
+          </Text>
+          <TouchableOpacity onPress={()=>openPicker('video')}>
+            {form.video ? (
+              <Video 
+                source={{uri: form.video.uri}}
+                className="w-full h-64 rounded-2xl"
+                useNativeControls
+                resizeMode={ResizeMode.COVER}
+                isLooping
+              />
+            ) : (
+              <View className="w-4 -40 px-4 bg-black-100 rounded-2xl justify-center items-center ">
+                <View className="w-14 h-14 border-dashed border-secondary-100 justify-center items-center">
+                <Image
+                source={icons.upload}
+                resizeMode="contain"
+                className="w-1/2"
+                />
+                </View>
+              </View>
+            )}
+          </TouchableOpacity>
+        </View>
+        <View className="mt-7 space-y-2 ">
+        <Text className="text-base text-gray-100 font-pmedium">
+            Thumbnial Image
+          </Text>
+          <TouchableOpacity onPress={()=>openPicker('image')}>
+            {form.thumbnail ? (
+              <Image
+                source={{uri: form.thumbnail.uri}}
                 resizeMode="cover"
                 className="w-full h-64 rounded-2xl"
               />
