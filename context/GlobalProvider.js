64c863301741408b9dbe3e386125b689d399cefe
Ajiboye <ajiboyemuyideen7@gmail.com>
2025-03-14 21:52:28 +0100

    updated

--- context/GlobalProvider.js
+++ context/GlobalProvider.js
@@ -14,7 +14,6 @@ export const GlobalProvider = ({ children }) => {
 
 
   useEffect(() => {
-    // Call the getCurrentUser function
      getCurrentUser()
       .then((res) => {
         if (res) {
